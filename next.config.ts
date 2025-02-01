2f71f4f865f4f8d10e8877e7ba4b6b52882e4c15
kronotitans <kronotitans@gmail.com>
2025-02-01 13:04:52 +0500

    Initial commit

---
+++ next.config.ts
@@ -0,0 +1,13 @@
+import nextra from "nextra";
+
+/** @type {import('next').NextConfig} */
+const withNextra = nextra({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+  defaultShowCopyCode: true,
+});
+
+export default withNextra({
+  output: "export",
+  images: { unoptimized: true },
+});
---
+++ theme.config.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  logo: <span>Whitepaper | Krono Titans</span>,
+  project: {
+    link: "https://github.com/kronotitans/krono-titans-docs",
+  },
+  docsRepositoryBase: "https://github.com/kronotitans/krono-titans-docs",
+  footer: {
+    content: "Krono Titans | KTN",
+  },
+};
+
+export default config;
