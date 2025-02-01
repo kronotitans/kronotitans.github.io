df2c0969fb9b59c0c4c1052135cc08e29385b970
kronotitans <kronotitans@gmail.com>
2025-02-01 18:48:17 +0500

    SEO added

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,11 +2,24 @@ import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="description" content="Krono Titans Whitepaper" />
+      <meta name="author" content="Krono Titans" />
+      <meta
+        name="keywords"
+        content="Krono Titans, KTN, KronoTitans Crypto, Krono Titans Blockchain"
+      />
+      <meta name="robots" content="index, follow" />
+      <title>Krono Titans Whitepaper | KTN</title>
+    </>
+  ),
   logo: <span>Whitepaper | Krono Titans</span>,
   project: {
-    link: "https://github.com/kronotitans/krono-titans-docs",
+    link: "https://github.com/kronotitans/kronotitans.github.io",
   },
-  docsRepositoryBase: "https://github.com/kronotitans/krono-titans-docs",
+  docsRepositoryBase: "https://github.com/kronotitans/kronotitans.github.io",
   footer: {
     content: "Krono Titans | KTN",
   },
